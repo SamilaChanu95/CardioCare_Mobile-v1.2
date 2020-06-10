052332d764e65f50afcaf8b5e55c97d3abea4dc3
SamilaChanu95 <chanukasamila@gmail.com>
2020-06-10 16:55:32 +0530

    Add new empty activity and layout.

---
+++ app/src/main/java/com/example/cardiologyapp/MainScreenActivity.java
@@ -0,0 +1,14 @@
+package com.example.cardiologyapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_screen);
+    }
+}
